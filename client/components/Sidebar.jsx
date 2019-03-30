ed1f3a39b604fef0c35bb2ff68005b15043a081b
Brayton Holman <brayton.holman@gmail.com>
2019-03-30 13:53:42 -0400

    Added tests and component for Sidebar (#11)

---
+++ client/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function Sidebar(props) {
+  return (
+    <div className="sidebar">
+      <h3>Active Users</h3>
+    </div>
+  );
+}
+
+export default Sidebar;
